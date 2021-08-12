918ce648fbe161df956359434aa29ca2ac329165
Jansen M Smith <JansenSmith@users.noreply.github.com>
2021-08-12 17:04:30 -0400

    inserted test rooms
    testing visualization

--- SecondFloor-3D.groovy
+++ SecondFloor-3D.groovy
@@ -1,1 +1,36 @@
-//Your code here
+//Based on http://localhost:60408/JavaCAD/SVGExtrude/index.html
+
+
+import eu.mihosoft.vrl.v3d.svg.*;
+
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Extrude;
+import eu.mihosoft.vrl.v3d.Polygon
+
+File f = ScriptingEngine
+	.fileFromGit(
+		"https://github.com/JansenSmith/41-dover.git",//git repo URL
+		"master",//branch
+		"SecondFloor.SVG"// File from within the Git repo
+	)
+println "Extruding SVG "+f.getAbsolutePath()
+SVGLoad s = new SVGLoad(f.toURI())
+println "Layers= "+s.getLayers()
+// A map of layers to polygons
+HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
+// extrude all layers to a map to 10mm thick
+HashMap<String,ArrayList<CSG>> csgByLayers = s.extrudeLayers(10)
+
+// extrude just one layer to 10mm
+def holeParts = s.extrudeLayerToCSG(10,"Floors")
+
+/*
+// seperate holes and outsides using layers to differentiate
+def outsideParts = s.extrudeLayerToCSG(10,"2-outsides")
+					.difference(holeParts)
+// layers can be extruded at different depths					
+def boarderParts = s.extrudeLayerToCSG(5,"3-boarder")
+*/
+//return [CSG.unionAll([boarderParts,outsideParts]),s.extrudeLayerToCSG(2,"4-star")]
